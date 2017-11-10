c3b249bff2e03af1555fdf4e6d8afa9ba9c689f0
vitaliy.khodyrev <32811396+obzor-time@users.noreply.github.com>
2017-11-10 18:27:09 +0600

    Add files via upload

---
+++ form/mail.php
@@ -0,0 +1,45 @@
+<?php 
+$formone = $_POST['form-one'];
+$username = $_POST['username'];
+$email = $_POST['femail'];
+$password = $_POST['password'];
+
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'vkhod_vit.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'vkhod_vit@mail.ru';                 // SMTP username
+$mail->Password = '12345678';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('vkhod_vit@mail.ru', 'Vitaliy Khodyrev');
+$mail->addAddress('vkhod_vit@mail.ru', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Тема сообщения';
+$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
+$mail->AltBody = 'Это сообщение в формате plain text';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} 
+
+else {
+    header('location: thanks.html');
+}
+
+?>
